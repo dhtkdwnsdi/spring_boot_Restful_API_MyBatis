586af812924c39d9a114b851db6f2d7c63fbb22e
osj950329 <dhtkdwnsdi@gmail.com>
2022-06-11 02:03:40 +0900

    first commit

---
+++ DevPorject/src/main/java/org/hdcd/DevPorjectApplication.java
@@ -0,0 +1,13 @@
+package org.hdcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPorjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPorjectApplication.class, args);
+	}
+
+}
